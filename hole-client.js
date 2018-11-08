6b97f2a4359ac24dd828a5e6853e8bf226a34a53
bobo <bobo@bobo.com>
2018-11-08 15:15:05 +0800

    1.中文提示方便自己记忆 2.增加一个简单的数据错误处理（转发时对应的socket不存在时重置缓存）

--- hole-client.js
+++ hole-client.js
@@ -1,16 +1,39 @@
 const net = require('net');
 const sp = require('./stickPackage');
-
-const OUT_SERVER_IP = '127.0.0.1';
-const OUT_SERVER_PORT = 8090;
-const IN_SERVER_IP = '127.0.0.1';
-const IN_SERVER_PORT = 8080;
+var argv = process.argv.splice(2);
+console.log(argv)
+var OUT_SERVER_IP = '127.0.0.1';
+var OUT_SERVER_PORT = 8071;
+var IN_SERVER_IP = '127.0.0.1';
+var IN_SERVER_PORT = 8090;
+var i = 0
+if (argv[0] !== undefined && argv[0] !== "" && parseInt(argv[0])) {
+    IN_SERVER_IP = argv[0]
+}
+if (argv[1] !== undefined && argv[1] !== "" && parseInt(argv[1])) {
+    IN_SERVER_PORT = parseInt(argv[1])
+}
+if (argv[2] !== undefined && argv[2] !== "" && parseInt(argv[2])) {
+    OUT_SERVER_IP = argv[2]
+}
+if (argv[3] !== undefined && argv[3] !== "" && parseInt(argv[3])) {
+    OUT_SERVER_PORT = parseInt(argv[3])
+}
+console.log(IN_SERVER_IP)
+console.log('usage: [被代理的服务器IP] [被代理的服务端口] [管道服务器IP] [管道服务端口] \n');
+console.log('default: 127.0.0.1 8090 127.0.0.1 8071 \n');
+console.log('被代理的服务: ' + IN_SERVER_IP +':'+ IN_SERVER_PORT);
+console.log('管道服务: ' + OUT_SERVER_IP + ':' + OUT_SERVER_PORT);
 
 const outclient = new net.Socket();
+outclient.setTimeout(3000);
+var timeout
 const inclientMap = {};
+var chunkRevNum = 0
+var chunkSendNum = 0
 
 outclient.connect(OUT_SERVER_PORT, OUT_SERVER_IP, function () {
-    console.log('outclient is connected.');
+    console.log('管道已连接.');
 });
 let halfUint8 = new Uint8Array();
 outclient.on('data', (data) => {
@@ -18,6 +41,7 @@ outclient.on('data', (data) => {
     halfUint8 = new Uint8Array();
     packs.forEach(pack => {
         if (pack.type === 'full') {
+            console.log(`代理连接 ${pack.tag} 发送数据 ${++chunkSendNum}` );
             buildClient(pack.tag);
             inclientMap[pack.tag] && inclientMap[pack.tag].write(Buffer.from(pack.payload));
         } else if (pack.type === 'half') {
@@ -25,28 +49,52 @@ outclient.on('data', (data) => {
         }
     });
 });
-outclient.on('close', () => console.log('outclient closed'));
-outclient.on('error', console.error);
 
-
+outclient.on('close', () => {
+    clearTimeout(timeout)
+    timeout = setTimeout(() => {
+        console.log('管道连接断开\n')
+        outclient.connect(OUT_SERVER_PORT, OUT_SERVER_IP);
+    }, 5000)
+});
+outclient.on('connect', function() {
+    console.log('管道连接/重连')
+});
+outclient.on('error', (err) => console.log(err));
+outclient.on('timeout', () => {
+    outclient.write('w')
+    console.log('管道连接超时')
+});
+setInterval(() => {
+    outclient.write('w')
+},3000)
 const buildClient = (addr) => {
     if (!inclientMap[addr]) {
         const inclient = new net.Socket();
+        console.log(`创建代理连接 ${addr}`);
         inclientMap[addr] = inclient;
 
         inclient.connect(IN_SERVER_PORT, IN_SERVER_IP, function () {
-            console.log(`inclient ${addr} is connected`);
+            console.log(`代理连接 ${addr} 连接成功`);
         });
         inclient.on('data', (data) => {
+            console.log(`代理连接 ${addr} 接收数据 ${++chunkRevNum} : ${data.length
+            }`);
             outclient.write(Buffer.from(sp.encode(addr, data)));
         });
         inclient.on('close', () => {
             delete inclientMap[addr];
-            console.log(`inclient ${addr} closed`);
+            console.log(`代理连接 ${addr} 关闭`);
         });
         inclient.on('error', function (err) {
+            console.log(`代理连接 ${addr} 异常`);
             console.error(err)
         });
     }
 }
 
+process.on('uncaughtException', (err) => {
+  console.log("======uncaughtException_start")
+  console.log(err)
+  console.log("======uncaughtException_end")
+});
--- hole-server.js
+++ hole-server.js
@@ -1,46 +1,90 @@
 const net = require('net');
 const sp = require('./stickPackage')
+var argv = process.argv.splice(2);
+console.log(JSON.stringify(argv))
+var IN_CHANNEL_PORT = 8071;
+var OUT_CHANNEL_PORT = 8070;
 
-const IN_CHANNEL_PORT = 8090;
-const OUT_CHANNEL_PORT = 8080;
+if (argv[0] !== undefined && argv[0] !== "" && parseInt(argv[0])) {
+    OUT_CHANNEL_PORT = parseInt(argv[0])
+}
+if (argv[1] !== undefined && argv[1] !== "" && parseInt(argv[1])) {
+    IN_CHANNEL_PORT = parseInt(argv[1])
+}
+console.log('usage: [外网服务端口] [管道服务端口] \n');
+console.log('default: 8070 8071 \n');
 
 let insock = null;
 let outsockMap = {};
+var chunkRevNum = 0
+var chunkSendNum = 0
+var heartBeat = 0
 
 const inserver = net.createServer((sock) => {
+    sock.setKeepAlive(true,3000)
     insock = sock;
-    let halfUint8 = new Uint8Array();
+    var halfUint8 = new Uint8Array();
     sock.on('data', function (data) {
-        const packs = sp.decode(Uint8Array.of(...halfUint8, ...data));
+        if (data.length ===1 && data.toString() === "w"){
+            console.error(`管道数据心跳包 ${++heartBeat} `)
+            if (sock.tureSocket === undefined) {
+                sock.tureSocket = true
+            }
+            return;
+        }
+        if (insock === undefined || !insock.tureSocket) {
+            insock = sock
+            return;
+        }
+        console.log("---管道数据接收"  + ++chunkRevNum )
+        var packs
+        try {
+            packs = sp.decode(Uint8Array.of(...halfUint8, ...data));
+        } catch(err) {
+            halfUint8 = new Uint8Array()
+            return
+        }
+        console.log(packs[0] != undefined ? packs[0].tag : "")
+        console.log(packs[0] != undefined ? packs[0].sum : "")
+        //console.log("---管道数据接收" + packs[0] != undefined ? packs[0].tag : "" + ":" + ++chunkRevNum)
         halfUint8 = new Uint8Array();
         packs.forEach(pack => {
             if(pack.type === 'full'){
             	try{
+                    console.log(pack.tag)
                 	outsockMap[pack.tag] && outsockMap[pack.tag].write(Buffer.from(pack.payload));
             	}catch(err){
-					console.error(`outsock ${pack.tag} broken`,err)
+					console.error(`管道数据接收异常 ${pack.tag} broken`,err)
             	}
             }else if(pack.type === 'half'){
-                halfUint8 = pack.bytes;
+                if (outsockMap[pack.tag]) {
+                    halfUint8 = pack.bytes;
+                } else {
+                    halfUint8 = new Uint8Array();
+                    console.error(`管道数据接收异常 ${pack.tag} broken pack length ${pack.sum}`)
+                }
             }
         });
     });
-
-
     sock.on('error', function (err) {
+        console.log('管道服务异常')
         console.error(err)
     });
     sock.on('close', function () {
         insock = null;
-        console.log('in closed')
+        console.log('管道客户端端口连接')
     });
 });
+inserver.on('connection', function (socket) {
+    console.log('管道服客户端上线')
+});
 inserver.listen(IN_CHANNEL_PORT, () => {
-    console.log(`inserver channel listen to ${IN_CHANNEL_PORT}`);
+    console.log('客户端管道连接端口: ' + IN_CHANNEL_PORT + "\n");
+    //console.log(`inserver channel listen to ${IN_CHANNEL_PORT}`);
 });
 
 const outserver = net.createServer((sock) => {
-	console.log(`new connection, ${sock.remoteFamily}#${sock.remoteAddress}:${sock.remotePort}`)
+	console.log(`外网服务器客户连接, ${sock.remoteFamily}#${sock.remoteAddress}:${sock.remotePort}`)
     let ipv4 = sock.remoteFamily === 'IPv4' && sock.remoteAddress ;
     if(!ipv4){
     	if(sock.remoteAddress && sock.remoteAddress.indexOf('::ffff:') > -1){
@@ -54,9 +98,12 @@ const outserver = net.createServer((sock) => {
     sock.on('data', function (data) {
         const buf = Buffer.from(sp.encode(addr,data));
         try{
-        	insock && insock.write(buf);
+        	if (insock) {
+                console.log("--管道数据发送" + addr + ":" + ++chunkSendNum)
+                insock.write(buf)
+            };
         }catch(err){
-        	console.error('insock broken',err)
+        	console.error('管道数据发送异常',err)
         }
     });
 
@@ -65,10 +112,16 @@ const outserver = net.createServer((sock) => {
     });
     sock.on('close', function () {
         delete outsockMap[addr];
-        console.log(`outsock ${addr} closed`);
+        console.log(`外网服务器客户断开 ${addr} close` );
     });
 });
 outserver.listen(OUT_CHANNEL_PORT, () => {
-    console.log(`outserver channel listen to ${OUT_CHANNEL_PORT}`);
+    console.log('外网服务器应用端口: ' + OUT_CHANNEL_PORT + "\n");
+    //console.log(`outserver channel listen to ${OUT_CHANNEL_PORT}`);
 });
 
+process.on('uncaughtException', (err) => {
+  console.log("======uncaughtException_start")
+  console.log(err)
+  console.log("======uncaughtException_end")
+});
